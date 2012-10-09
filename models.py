c2607be58b9a01a56635b6cd99f90c3fdb5da842
John Schimmel <john.schimmel@gmail.com>
2012-10-09 13:35:36 -0400

    weird main.html character removed

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from mongoengine import *
 from flask.ext.mongoengine.wtf import model_form
 from datetime import datetime
 
+# our demo model from week 5 in class
 class Log(Document):
 	text = StringField()
 	timestamp = DateTimeField(default=datetime.now())
